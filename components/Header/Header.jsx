e666a75de3f999091cd2e0ec1d4b00086e79a2e0
SametYurtoglu <yrtlsamet@gmail.com>
2023-02-15 16:31:09 +0300

    Header Update 🔥

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,54 +1,71 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
-import { AiFillCloseCircle, AiOutlineClose, AiOutlineMenuUnfold } from 'react-icons/ai'
-import OutsideClickHandler from 'react-outside-click-handler';
-import {BsDownload} from 'react-icons/bs'
+import { AiFillCloseSquare, AiOutlineMenu } from 'react-icons/ai'
 
 const Header = () => {
     const router = useRouter()
     const [nav, setnav] = useState(false)
     return (
-        <div>
-            <nav className="w-full h-[6rem] flex justify-between items-center container mx-auto">
-                <div>
-                    <Link href='/pages/PlusBot' className='text-[#a8ada9] hover:text-white font-semibold text-3xl'>PlusBot</Link>
-                </div>
-                <div>
-                    <ul className='hidden md:flex  gap-5 text-gray-400  font-medium'>
-                        <Link href="/" className='' legacyBehavior>
-                            <a className={router.pathname === '/' ? 'underline  text-white' : ''}>Home</a>
-                        </Link>
-                        <Link href="/Commands" legacyBehavior>
-                            <a className={router.pathname === '/Commands' ? 'underline  text-white' : ''}>Komutlar</a>
-                        </Link>
-                    </ul>
-                </div>
-
-                <div className='flex gap-5'>
-                    <button className='py-1 px-2 rounded-full text-white text-xl hover:bg-[#191a19] md:hidden flex' onClick={() => setnav(true)}><AiOutlineMenuUnfold /></button>
-                    <button className='py-1 px-2 rounded-full text-white text-xl hover:bg-[#191a19] animate-bounce'><BsDownload /></button>
-                </div>
-            </nav>
-            {nav &&
-                <OutsideClickHandler onOutsideClick={() => setnav(false)}>
-                    <div className='absolute z-50 bg-blue-700/20 w-full h-[200px] top-20 left-0 md:hidden'>
-                        <div className="flex justify-center items-center h-full text-center">
-                            <ul className='flex flex-col gap-2'>
-                                <Link href="/" className='' legacyBehavior>
-                                    <a className={router.pathname === '/' ? 'underline  text-white' : ''}>Home</a>
-                                </Link>
-                                <Link href="/Commands" legacyBehavior>
-                                    <a className={router.pathname === '/Commands' ? 'underline  text-white' : ''}>Komutlar</a>
-                                </Link>
+        <div className='container mx-auto '>
+            <div className="flex justify-between items-center h-[6rem] w-full">
+                <div className="flex gap-10 items-center">
+                    <h1 className='text-3xl font-bold'>
+                        <span className='text-blue-600'>Plus</span>
+                        <span className='text-white'>Bot</span>
+                    </h1>
+                    <div className="">
+                        <ul className='hidden md:flex gap-3 text-[15px] font-medium'>
+                            <li>
+                                <Link href={'/'} className={router.pathname === '/' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Home</Link>
+                            </li>
+                            <li>
+                                <Link href={'/Commands'} className={router.pathname === '/Commands' ? 'text-white underline underline-offset-8 decoration-blue-600 ' : 'text-blue-500 hover:text-blue-600'}>Commands</Link>
+                            </li>
+                            <li>
+                                <Link href={'/Vote'} className={router.pathname === '/Vote' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Vote</Link>
+                            </li>
+                            <li>
+                                <Link href={'/addbot'} className={router.pathname === '/addbot' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Add Bot</Link>
+                            </li>
+                            <li>
+                                <Link href={'/addbot'} className={router.pathname === '/addbot' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>About</Link>
+                            </li>
+                            <li>
+                                <Link href={'/addbot'} className={router.pathname === '/addbot' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Sponsor</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    {nav && <div className=''>
+                        <div className="bg-gray-800 w-full h-[160px] flex md:hidden top-[70px] left-0 absolute justify-center">
+                            <ul className='flex flex-col justify-center text-center h-full gap-1 text-[15px] font-medium'>
+                                <li>
+                                    <Link href={'/'} className={router.pathname === '/' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Home</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/Commands'} className={router.pathname === '/Commands' ? 'text-white underline underline-offset-8 decoration-blue-600 ' : 'text-blue-500 hover:text-blue-600'}>Commands</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/Vote'} className={router.pathname === '/Vote' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Vote</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/addbot'} className={router.pathname === '/addbot' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Add Bot</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/addbot'} className={router.pathname === '/addbot' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>About</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/addbot'} className={router.pathname === '/addbot' ? 'text-white' : 'text-blue-500 hover:text-blue-600'}>Sponsor</Link>
+                                </li>
                             </ul>
                         </div>
-                        <button className='absolute top-2 right-2 text-white text-xl' onClick={() => setnav(false)}><AiFillCloseCircle /></button>
-                    </div>
-                </OutsideClickHandler>
-            }
-
-
+                    </div>}
+                </div>
+                <div className="flex items-center gap-2 ">
+                    <button className='text-white flex md:hidden bg-blue-600/20 hover:bg-blue-600/40 rounded-lg p-2' onClick={() => setnav(!nav)}><AiOutlineMenu/></button>
+                    <button className='text-white font-medium animate-bounce'>add a bot</button>
+                </div>
+            </div>
         </div>
     )
 }
