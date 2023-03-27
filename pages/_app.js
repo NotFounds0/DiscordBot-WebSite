a9ec0d828c403830a3c32f516de8c5caef93a5c9
SametYurtoglu <yrtlsamet@gmail.com>
2023-03-27 21:26:26 +0300

    Update ✅

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import Loading from '@/components/Loading';
-import '@/styles/globals.css'
-import { useEffect, useState } from 'react';
-import Layout from '../layout'
+import Loading from "@/components/Loading";
+import Router from "next/router";
+import NProgress from "nprogress";
+import '@/styles/globals.css';
+import "nprogress/nprogress.css";
+import { useEffect, useState } from "react";
+import Layout from "./../layout.js";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
